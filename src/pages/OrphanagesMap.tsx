b69cb3a7ee9f3b046fda8f3eb6319400d7579f27
Renato  Avellar <onizuka@Mac-mini.local>
2020-10-12 13:00:10 -0300

    estrutura e css da pagina de mapas

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import mapMarker from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanegeMap(){
     return(
-        <h1>Hello</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt="logo"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Santos</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="createOrphanage">
+                <FiPlus  size={32} color="#FFF"/>
+            </Link>
+        </div>
     );
 }
 
