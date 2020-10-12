20916270d50833f3ab1b8d1b14a674ed15f6f74b
Renato  Avellar <onizuka@Mac-mini.local>
2020-10-12 11:57:30 -0300

    transformando a Landing em componente

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import Landing from './pages/Landing';
+
+function Routes(){
+    return (
+        <BrowserRouter>
+           
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
